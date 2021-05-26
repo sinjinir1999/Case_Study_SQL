4951b0f17df9b170ce07c5795c0572efe1438f23
Sinjini Roy <67474818+sinjinir1999@users.noreply.github.com>
2021-05-26 21:11:27 +0530

    Delete I.sql

--- I.sql
+++
@@ -1,8 +0,0 @@
-WITH CTE_Rides AS
-(SELECT MONTHNAME(created_AT) AS month_name,
-YEAR(created_at) AS year_name,
-COUNT (*) AS rides
-FROM ride_details
-GROUP BY month_name,year_name)
-SELECT CONCAT(CAST(month_name AS CHAR), CAST(year_name AS CHAR)) AS month_year, rides
-FROM CTE_Rides;
